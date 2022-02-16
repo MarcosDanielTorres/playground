f75385223d8f6b172c98a932997d38a2ea468ea0
Marcos Daniel Torres <mdaniel.torres7@gmail.com>
2022-02-16 20:00:31 -0300

    nada

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,20 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="app__footer">
+        <ul className='app__footer-links'>
+          <li>Privacy Policy</li>
+          <li>Buy $LMAO Now</li>
+          <li>Live Chart</li>
+        </ul>
+
+        <div className="app__footer-copyright">
+          <b>Copyright</b>
+          <p>2021 LMAO</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 
 import './Navbar.css';
 import logo from '../../assets/logo.png'
@@ -9,14 +10,14 @@ const Navbar = () => {
         <div className="app__navbar-logo">
             <img src={logo} alt="lmao logo" />
         </div>
-        <ul className="app__navbar-links">
-            <li >How To Buy</li>
-            <li >Whitepaper</li>
-            <li >Moon Map</li>
-            <li >Tokenomics</li>
-            <li >Contact Us</li>
-            <li><button type="button" className='custom__button'>Buy $LMAO</button></li>
-        </ul>
+            <ul className="app__navbar-links">
+                <li >How To Buy</li>
+               <li >Whitepaper</li>
+                <li >Moon Map</li>
+                <li >Tokenomics</li>
+                <li >Contact Us</li>
+            </ul>
+            
       </nav>
   )
 }
